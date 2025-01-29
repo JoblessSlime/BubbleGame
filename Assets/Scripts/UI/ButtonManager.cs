dfeac20adbc71e3e1bbd0d33ef2cc58fddf1e840
Yuu-NiKorn <yuuyunikorn@gmail.com>
2025-01-29 11:45:37 +0100

    pou

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameManager gameManager;
+
+    [SerializeField] ScriptableObject gameManager;
+    [SerializeField] GameManager gameManager;
+    [SerializeField] GameManager gameManager;
+    [SerializeField] GameManager gameManager;
+    [SerializeField] GameManager gameManager;
+    [SerializeField] GameManager gameManager;
+    [SerializeField] GameManager gameManager;
+    [SerializeField] GameManager gameManager;
+    [SerializeField]
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PopUp()
+    {
+
+    }
+}
